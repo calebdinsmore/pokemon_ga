81a269aed8e49258ba33f45f63eded50ea42aece
cjdinsmore <caleb_dinsmore@taylor.edu>
2016-04-12 18:13:56 -0400

    Added some stuff

--- Pokemon.py
+++ Pokemon.py
@@ -2,5 +2,13 @@
 """Class file defining Pokemon"""
 
 class Pokemon(object):
-    def __init__(self, id, name, types, stats, valid_moves, moves):
-        pass
+    def __init__(self, pokeId, name, types, stats, valid_moves, moves):
+        self.id = pokeId
+        self.name = name
+        self.types = types
+        self.hp = stats['hp']
+        self.attack = stats['attack']
+        self.defense = stats['defense']
+        self.sp_attack = stats['spattack']
+        self.sp_defense = stats['spdefense']
+        self.speed = stats['speed']
